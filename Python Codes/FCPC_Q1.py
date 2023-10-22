ce7894922c2e8815ad2e67ade9af7eb88de06b94
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-22 21:11:13 +0530

    python code solved

--- Python Codes/FCPC_Q1.py
+++ Python Codes/FCPC_Q1.py
@@ -6,7 +6,7 @@ else:
     print(year,"is not a leap year")
 
 
-#Exception for solar year (optimized code )
+# Exception for solar year (optimized code )
 year = int(input("Enter year:"))
 
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
