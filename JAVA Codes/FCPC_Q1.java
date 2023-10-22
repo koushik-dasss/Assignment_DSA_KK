0cd4deb85264b191aac2692d504e68e0bb79dcca
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-22 20:54:49 +0530

    first java code ran succesfully in vscode for pushing into github

--- JAVA Codes/FCPC_Q1.java
+++ JAVA Codes/FCPC_Q1.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class FCPC_Q1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer:");
+        int k = sc.nextInt();
+        System.out.println("int is:" + k);
+         
     }
 }
